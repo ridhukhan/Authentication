c8825c2562982a441e0177f3e2a4f6d4ed6701ad
ridhukhan <prakritirshandhane@gmail.com>
2026-02-17 23:44:12 +0600

    update

--- .history/frontend/src/pages/Register_20260217232845.jsx
+++ .history/frontend/src/pages/Register_20260217232845.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useState } from 'react'
-import api from '../apiinterchapter'
 import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 const Register = () => {
