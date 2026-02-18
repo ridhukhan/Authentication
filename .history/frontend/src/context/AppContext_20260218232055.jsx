26fb6bfdd7740f383c98cd096c117ada0eb6cddf
ridhukhan <prakritirshandhane@gmail.com>
2026-02-18 23:33:47 +0600

    update

---
+++ .history/frontend/src/context/AppContext_20260218232055.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { createContext } from "react";
+import { useEffect } from "react";
+import { useContext } from "react";
+import api from "../apiinterchapter";
+import { toast } from "sonner";
+const AppContext=createContext(null)
+export const AppProvider=({children})=>{
+    const [user,setUser]=useState(null)
+    const [loading ,setLoading]=useState(true)
+    const [isAuth ,setIsAuth]=useState(false)
+    async function fetchUser(){
+
+        setLoading(true)
+        try {
+            const {data}=await api.get(`/user/me`);
+            setUser(data.user)
+            setIsAuth(true)
+        } catch (error) {
+       console.log(error)
+       setUser(null)
+       setIsAuth(false)
+        }finally{
+            setLoading(false)
+        }
+    }
+
+    async function logoutUser(navigate){
+        try {
+            const {data}= await api.post("/api/user/logout")
+            toast.success(data.message)
+            setIsAuth(false)
+            setUser(null)
+            navigate("/login")
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+    useEffect(()=>{
+        fetchUser()
+    },[])
+return(<AppContext.Provider value={{setIsAuth,isAuth,user,setUser,loading,logoutUser}}>{children}</AppContext.Provider>
+);
+};
+export const AppData=()=>{
+    const context=useContext(AppContext)
+    if(!context) throw new Error("AppData must be used Appprovider");
+    return context;
+}
--- .history/frontend/src/pages/login_20260218232409.jsx
+++ .history/frontend/src/pages/login_20260218232409.jsx
@@ -1,28 +1,23 @@
 import React from 'react'
 import { useState } from 'react'
-import api from '../apiinterchapter'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
-const Register = () => {
-    const [fullname,setFullname]=useState("")
-
+import api from '../apiinterchapter'
+const Login = () => {
     const [email,setEmail]=useState("")
     const [password,setPassword]=useState("")
     const [loading,setLoading]=useState(false)
-    
+    const navigate =useNavigate()
 const submithandler=async (e)=>{
     setLoading(true)
     e.preventDefault();
 try {
-    const {data}=await api.post("/user/register",{
-      fullname,
+    const {data}= await api.post("/api/user/login",{
         email,
         password})
     toast.success(data.message)
-
-   setFullname("")
-   setEmail("")
-   setPassword("")
+    localStorage.setItem("email",email)
+    navigate("/verifyotp")
 } catch (error) {
     toast.error(error.message)
 }finally{
@@ -39,15 +34,7 @@ try {
     <form onSubmit={submithandler} className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col
      md:ml-auto w-full mt-10 md:mt-0">
       <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Login</h2>
-      <div className="relative mb-4">
-        <label htmlFor="fullname" className="leading-7 text-sm text-gray-600">Full Name</label>
-        <input type="text" 
-        id="fullname"
-         name="fullname"
-          value={fullname}
-           onChange={(e)=>setFullname(e.target.value)} 
-           className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
-      </div>
+      
       <div className="relative mb-4">
         <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
         <input type="email" 
@@ -67,8 +54,10 @@ try {
          text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
       </div>
       <button   className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-       disabled={loading}>{loading?"submitting data.......":"REGISTER"}</button>
-   <Link to={"/login"} className='text-amber-700'>Don't have an Account??</Link>
+       disabled={loading}>{loading?"submitting data.......":"LOGIN"}</button>
+
+<p onClick={()=>navigate("/forgot-password")}>forgot password??</p>
+   <Link to={"/register"} className='text-amber-700'>Don't have an Account??</Link>
     </form>
   </div>
 </section>
@@ -76,4 +65,4 @@ try {
   )
 }
 
-export default Register
+export default Login
