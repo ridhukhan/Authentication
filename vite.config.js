975e188a5df0c8057aac7830998dbe5f6a6a9f48
ridhukhan <prakritirshandhane@gmail.com>
2026-02-19 05:23:09 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  root:'frontend',
   plugins: [react(),
     tailwindcss()
-  ]
+  ],
+  build:{
+    outDir:'../dist'
+  }
 })
