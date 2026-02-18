bfbf6b83352ddf82d98904eba8081418eb352b06
ridhukhan <prakritirshandhane@gmail.com>
2026-02-19 05:42:02 +0600

    update

---
+++ .history/vite.config_20260219054118.js
@@ -0,0 +1,56 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  root: 'frontend',
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist', // Eita 'frontend/dist' e output pathabe
+    emptyOutDir: true,
+  }
+})import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  root: 'frontend',
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist', // Eita 'frontend/dist' e output pathabe
+    emptyOutDir: true,
+  }
+})import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  root: 'frontend',
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist', // Eita 'frontend/dist' e output pathabe
+    emptyOutDir: true,
+  }
+})import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  root: 'frontend',
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist', // Eita 'frontend/dist' e output pathabe
+    emptyOutDir: true,
+  }
+})import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  root: 'frontend',
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist', // Eita 'frontend/dist' e output pathabe
+    emptyOutDir: true,
+  }
+})
